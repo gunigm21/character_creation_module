22c003de49a6664a7a75854069242cbc4cf82764
Guzel Egorova <gunigm21@gmail.com>
2024-10-09 21:09:06 +0400

    oop run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from random import randint
+from graphic_arts.graphic_arts.start_game_banner import run_screensaver
+
 DEFAULT_ATTACK = 5
 DEFAULT_DEFENCE = 10
 DEFAULT_STAMINA = 80
@@ -86,3 +88,16 @@ def start_training(character):
         if cmd in commands:
             print(commands[cmd]())
     return 'Тренировка окончена.'
+
+if __name__ == '__main__':
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class)) 
+    
